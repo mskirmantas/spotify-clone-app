c0bf04d509a0a0981297af078e7510ec7f029133
mantasskirma <56951458+mantasskirma@users.noreply.github.com>
2020-01-28 16:42:21 +0200

    Save before ejecting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.scss";
+import Button from "antd/es/button";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Hello, Spotify</h1>
+      <Button type="primary">Button</Button>
     </div>
   );
 };
